39b1aa9337ac25015ec2cd0b8a283f03adc81ba7
Cristian_Valencia <u20231210808@usco.edu.co>
2025-04-09 15:02:51 -0500

    cambio de salpicadero

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,7 +8,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Sistema-auth') }}
                     </x-nav-link>
                 </div>
             </div>
